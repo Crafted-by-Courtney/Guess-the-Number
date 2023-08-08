9f3951a59b9112fece265a6dd3b47a6d41a02c75
Courtney <courtneycook82@gmail.com>
2023-08-08 15:19:36 -0400

    restructuring

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const guessButton = document.getElementById('guessButton');
+const guessInput = document.getElementById('guessInput');
+const message = document.getElementById('message');
+
 // Generate a random number between min (inclusive) and max (inclusive)
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,36 +14,27 @@ function guessTheNumberGame() {
   const targetNumber = generateRandomNumber(minNumber, maxNumber);
   let attempts = 0;
 
-  const guessButton = document.getElementById('guessButton');
-      const guessInput = document.getElementById('guessInput');
-      const message = document.getElementById('message');
-
-      guessButton.addEventListener('click', function () {
-        const playerGuess = parseInt(guessInput.value, 10);
-      }
-
-  while (true) {
-    const playerGuess = parseInt(prompt(`Guess the number between ${minNumber} and ${maxNumber}:`), 10);
+  guessButton.addEventListener('click', function () {
+    const playerGuess = parseInt(guessInput.value, 10);
 
     if (isNaN(playerGuess)) {
-      console.log("Please enter a valid number.");
-      continue;
+      message.textContent = "Please enter a valid number.";
+      return;
     }
 
     attempts++;
 
     if (playerGuess < targetNumber) {
-      console.log("Too low! Try again.");
+      message.textContent = "Too low! Try again.";
     } else if (playerGuess > targetNumber) {
-      console.log("Too high! Try again.");
+      message.textContent = "Too high! Try again.";
     } else {
-      console.log(`Congratulations! You guessed the number ${targetNumber} in ${attempts} attempts.`);
+      message.textContent = `Congratulations! You guessed the number ${targetNumber} in ${attempts} attempts.`;
       guessButton.disabled = true;
       guessInput.disabled = true;
-      break;
-      }
+    }
   });
 }
 
 // Start the game
-guessTheNumberGame(165);
+guessTheNumberGame();
