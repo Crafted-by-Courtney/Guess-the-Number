6c2c4df283f19afa4b0332e54c18d66d6473ad87
Courtney <courtneycook82@gmail.com>
2023-08-14 15:30:03 -0400

    Adding a maximum number of attempts

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const guessInput = document.getElementById('guessInput');
 const message = document.getElementById('message');
 const resetButton = document.getElementById('resetButton'); // Get the Reset button
 
+const maxAttempts = 10;
 let targetNumber, attempts;
 
 // Generate a random number between min (inclusive) and max (inclusive)
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+alert("Welcome to Guess the Number!");
+alert(`Try to guess the secret number between 1 and 100. You have ${maxAttempts} attempts.`);
 
 function resetGame() {
   targetNumber = generateRandomNumber(1, 100);
@@ -18,14 +22,8 @@ function resetGame() {
   guessInput.disabled = false;
 }
 
-
-// Main game function
-// function guessTheNumberGame() {
-//   resetGame(); // Initialize the game when the page loads
-//   const minNumber = 1;
-//   const maxNumber = 100;
-//   const targetNumber = generateRandomNumber(minNumber, maxNumber);
-//   let attempts = 0;
+const minNumber = 1;
+const maxNumber = 100;
 
 // Main game function
 function guessTheNumberGame() {
@@ -34,14 +32,17 @@ function guessTheNumberGame() {
   guessButton.addEventListener('click', function () {
     const playerGuess = parseInt(guessInput.value, 10);
 
-    if ((isNaN(playerGuess)) || (playerGuess> maxNumber)) {
+    // while (attempts <= maxAttempts) {
+      // message.textContent = "Enter your guess:";
+
+      if ((isNaN(playerGuess)) || (playerGuess> maxNumber)) {
       message.textContent = "Please enter a valid number.";
       return;
-    }
+     }
 
     attempts++;
 
-    if (playerGuess < targetNumber) {
+    if ((playerGuess < targetNumber) && (attempts <= maxAttempts)) {
       message.textContent = "Too low! Try again.";
     } else if (playerGuess > targetNumber) {
       message.textContent = "Too high! Try again.";
@@ -49,12 +50,23 @@ function guessTheNumberGame() {
       message.textContent = `Congratulations! You guessed the number ${targetNumber} in ${attempts} attempts.`;
       guessButton.disabled = true;
       guessInput.disabled = true;
+    } 
+
+    if ((playerGuess !== targetNumber) && (attempts > maxAttempts)) {
+      message.textContent = `Sorry, you've run out of attempts. The secret number was ${targetNumber}. You lose!`;
     }
   });
-}
+  }
 
 resetButton.addEventListener('click', resetGame); // Add event listener for the Reset button
 
 
 // Start the game
 guessTheNumberGame();
+
+
+
+
+
+
+
