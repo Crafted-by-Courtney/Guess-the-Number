b362f4971ae78e84a4657a6ba2333ce8e01480c0
Courtney <courtneycook82@gmail.com>
2023-08-15 04:13:41 -0400

    Added score

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// document.getElementById() method is used to fetch an element from the HTML page having the id as provided (specified) by the user
 const guessButton = document.getElementById('guessButton');
 const guessInput = document.getElementById('guessInput');
 const message = document.getElementById('message');
 const resetButton = document.getElementById('resetButton'); // Get the Reset button
-
 const maxAttempts = 10;
-let targetNumber, attempts;
+const minNumber = 1;
+const maxNumber = 100;
+
+let targetNumber, attempts, score;
 
 // Generate a random number between min (inclusive) and max (inclusive)
 function generateRandomNumber(min, max) {
@@ -12,53 +15,65 @@ function generateRandomNumber(min, max) {
 }
 
 alert("Welcome to Guess the Number!");
+// The user is prompted to guess a number from 1 to 100
 alert(`Try to guess the secret number between 1 and 100. You have ${maxAttempts} attempts.`);
 
 function resetGame() {
-  targetNumber = generateRandomNumber(1, 100);
+  targetNumber = generateRandomNumber(1, maxNumber);
   attempts = 0;
+  score = 0;
+  updateScoreDisplay(); // Update the score display
   message.textContent = '';
   guessButton.disabled = false;
   guessInput.disabled = false;
 }
 
-const minNumber = 1;
-const maxNumber = 100;
+function updateScoreDisplay() {
+  document.getElementById('score').textContent = `Score: ${score}`;
+}
 
 // Main game function
 function guessTheNumberGame() {
   resetGame(); // Initialize the game when the page loads
 
   guessButton.addEventListener('click', function () {
+    // The parseInt() converts the numeric string value to an integer value
     const playerGuess = parseInt(guessInput.value, 10);
 
     // while (attempts <= maxAttempts) {
       // message.textContent = "Enter your guess:";
 
-      if ((isNaN(playerGuess)) || (playerGuess> maxNumber)) {
+      if ((isNaN(playerGuess)) || (playerGuess > maxNumber)) {
       message.textContent = "Please enter a valid number.";
       return;
      }
 
     attempts++;
 
+    // The if...else statement is used to check the condition. The equal to == operator is used to check if the guess was correct
     if ((playerGuess < targetNumber) && (attempts <= maxAttempts)) {
       message.textContent = "Too low! Try again.";
     } else if (playerGuess > targetNumber) {
       message.textContent = "Too high! Try again.";
-    } else {
+    } else if ((playerGuess === targetNumber) && (attempts <= maxAttempts)) {
+      score += 10; // Increase the score when the player guesses correctly
       message.textContent = `Congratulations! You guessed the number ${targetNumber} in ${attempts} attempts.`;
+      updateScoreDisplay();
       guessButton.disabled = true;
       guessInput.disabled = true;
     } 
 
-    if ((playerGuess !== targetNumber) && (attempts > maxAttempts)) {
+    if (attempts > maxAttempts) {
       message.textContent = `Sorry, you've run out of attempts. The secret number was ${targetNumber}. You lose!`;
+      guessButton.disabled = true;
+      guessInput.disabled = true;
+      score -= 5; // Subtract 5 from the score when the player loses
+      updateScoreDisplay();
     }
   });
   }
 
-resetButton.addEventListener('click', resetGame); // Add event listener for the Reset button
+resetButton.addEventListener('click', guessTheNumberGame); // Add event listener for the Reset button
 
 
 // Start the game
